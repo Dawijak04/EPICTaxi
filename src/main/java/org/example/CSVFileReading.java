4bb963b9b91bd82c165f605d4da47eb2fe9ec342
Dawijak04 <23384549@studentmail.ul.ie>
2023-12-04 13:55:31 +0000

    rating driver

--- src/main/java/org/example/CSVFileReading.java
+++ src/main/java/org/example/CSVFileReading.java
@@ -1,13 +1,15 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.example.UserInterface.*;
+
 public class CSVFileReading {
     static String csvPath = "D:\\EPICTaxi\\src\\TaxiFile.txt";
+    static String tempCsvPath = "D:\\EPICTaxi\\src\\TempFile.txt";
 
     private static List<Taxi> Taxis = new ArrayList<>();
 
@@ -22,7 +24,7 @@ public class CSVFileReading {
 
                 String[] data = line.split(",");
 
-                if (data.length >= 5) { // Ensure there are at least 5 columns
+                if (data.length >= 7) { // Ensure there are at least 5 columns
 
 
                     try {
@@ -31,11 +33,13 @@ public class CSVFileReading {
                         String make = data[2].trim();
                         String model = data[3].trim();
                         String driverName = data[4].trim();
+                        double rating = Double.parseDouble(data[5].trim());
+                        int noOfTrips = Integer.parseInt(data[6].trim());
 
                         //System.out.println(make);
 
 
-                        Taxis.add(new Taxi(type, reg, make, model, driverName));
+                        Taxis.add(new Taxi(type, reg, make, model, driverName, rating, noOfTrips));
 
                     } catch (NumberFormatException e) {
                         System.err.println("Error parsing age: " + e.getMessage());
@@ -54,8 +58,45 @@ public class CSVFileReading {
         }
         System.out.println(Taxis.get(11).getDriverName());
     }
+    public static void updateRating() {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvPath));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(tempCsvPath))) {
+            String line = null;
+            int currentLine = 0;
+            DecimalFormat roundToThree = new DecimalFormat("#.###");
+            double newRating = ((Person.getAssignedTaxi().getRating()* Person.getAssignedTaxi().getNoOfTrips()) +
+                    Double.parseDouble(UserInterface.getRating()))/
+                    (Person.getAssignedTaxi().getNoOfTrips() + 1);
+            newRating = Double.parseDouble(roundToThree.format(newRating));
 
-    public static List<Taxi> getTaxis() {
-        return Taxis;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                currentLine++;
+                if (data[1].equals(Person.getAssignedTaxiReg())) {
+                    line = line.replace(data[5], Double.toString(newRating));
+                }
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error with updating rating");
+            //e.printStackTrace();
+            //throw new RuntimeException(e);
+        }
+        File originalFile = new File(csvPath);
+        File tempFile = new File(tempCsvPath);
+        if (tempFile.renameTo(originalFile)) {
+            System.out.println("Replacement success");
+        } else {
+            System.out.println("Error with replacement");
+        }
     }
-}
+
+
+
+
+        public static List<Taxi> getTaxis () {
+            return Taxis;
+        }
+    }
+
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,8 +1,13 @@
 package org.example;
 import java.util.Random;
+
+
+
 public class Person {
     private int locX;
     private int locY;
+    private static String assignedTaxiReg = "12-RN-831";
+    private static Taxi assignedTaxi = CSVFileReading.getTaxis().get(3);
 
     public Person(){
         Random rand = new Random();
@@ -24,5 +29,21 @@ public class Person {
 
     public void setLocY(int locY) {
         this.locY = locY;
+    }
+
+    public static String getAssignedTaxiReg() {
+        return assignedTaxiReg;
+    }
+
+    public static void setAssignedTaxiReg(String reg) {
+        assignedTaxiReg = reg;
+    }
+
+    public static Taxi getAssignedTaxi() {
+        return assignedTaxi;
+    }
+
+    public static void setAssignedTaxi(Taxi assignedTaxi) {
+        Person.assignedTaxi = assignedTaxi;
     }
 }
