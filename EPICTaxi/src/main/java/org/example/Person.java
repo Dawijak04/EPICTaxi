6fa915cb4dc14eddfa9f160313486c745bd28c94
alisiak03 <145369992+alisiak03@users.noreply.github.com>
2023-12-09 16:09:58 +0000

    commiting local chhanges before pull

--- EPICTaxi/src/main/java/org/example/Person.java
+++ EPICTaxi/src/main/java/org/example/Person.java
@@ -7,13 +7,13 @@ public class Person {
     private int locX;
     private int locY;
 
-    private static Taxi assignedTaxi = CSVFileReading.getTaxis().get(15);
+   private static Taxi assignedTaxi = CSVFileReading.getTaxis().get(15);
     private static String assignedTaxiReg = assignedTaxi.getReg();
 
     public Person(int gridDimension){
         Random rand = new Random();
-        this.locX = rand.nextInt(10);
-        this.locY = rand.nextInt(10);
+        this.locX = rand.nextInt(gridDimension);
+        this.locY = rand.nextInt(gridDimension);
         this.locX = Math.min(Math.max(this.locX, 0), gridDimension - 1);
         this.locY = Math.max(Math.min(this.locY, 0), gridDimension - 1);
 
@@ -35,19 +35,17 @@ public class Person {
         this.locY = locY;
     }
 
-    public static String getAssignedTaxiReg() {
-        return assignedTaxiReg;
+   public static String getAssignedTaxiReg() {return assignedTaxiReg;}
+
+   public static void setAssignedTaxiReg(String reg) {
+       assignedTaxiReg = reg;
     }
 
-    public static void setAssignedTaxiReg(String reg) {
-        assignedTaxiReg = reg;
-    }
+   public static Taxi getAssignedTaxi() {
+       return assignedTaxi;
+   }
 
-    public static Taxi getAssignedTaxi() {
-        return assignedTaxi;
-    }
-
-    public static void setAssignedTaxi(Taxi assignedTaxi) {
-        Person.assignedTaxi = assignedTaxi;
-    }
+   public static void setAssignedTaxi(Taxi assignedTaxi) {
+       Person.assignedTaxi = assignedTaxi;
+   }
 }
