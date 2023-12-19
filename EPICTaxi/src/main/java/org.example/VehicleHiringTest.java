be57c9f6d0e4761a2fd46920349dcfb0a2a13af0
alisiak03 <145369992+alisiak03@users.noreply.github.com>
2023-12-19 20:55:10 +0000

    testing set up

---
+++ EPICTaxi/src/main/java/org.example/VehicleHiringTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public interface VehicleHiringTest {
+
+    public abstract void testAddToMap();
+
+    public abstract void testMoveVehicle();
+    public abstract void testRemoveVehicle();
+    public abstract void testGetVehicleLoc();
+    public abstract void testGetVehiclesInRange();
+}
+
--- EPICTaxi/src/test/Testing.java
+++ EPICTaxi/src/test/Testing.java
@@ -1,19 +1,67 @@
-
-//import org.junit.Test;
-
-import org.example.Main;
+import org.example.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+public class Testing  implements VehicleHiringTest {
 
 
 
-
-public class Testing  {
     @Test
     public void test1() {
         assertEquals((Main.add(1, 2)), 3);
     }
+    @Test
+    public boolean testAddToMap(String reg, int x , int y){
+        DataList<Taxi> generatedTaxis = Taxi.randomTaxiGenerate();
+        String registration = "151-C-935";
+
+        for(int i =0; i < generatedTaxis.size(); i++){
+            if(generatedTaxis.get(i).getReg().equals(registration)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    @Override
+    public void testAddToMap() {
+        //method that displays elements on the linked grid
+     LinkedGrid lg = new LinkedGrid(10);
+     lg.display();
+
+     int taxix = 3;
+     int taxiy = 4;
+     DataList<Taxi> generatedtaxis;
+    // lg.getTaxiAtPosition(taxix, taxiy, generatedtaxis);
 
 
 
+
+
+
+
+
+    }
+
+
+
+    @Override
+    public void testMoveVehicle() {
+
+    }
+
+    @Override
+    public void testRemoveVehicle() {
+
+    }
+
+    @Override
+    public void testGetVehicleLoc() {
+
+    }
+
+    @Override
+    public void testGetVehiclesInRange() {
+
+    }
 }
