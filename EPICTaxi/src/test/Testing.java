af2a723aa58c938f7eb5781b539bd4e6f5fa4c87
alisiak03 <145369992+alisiak03@users.noreply.github.com>
2023-12-19 16:54:59 +0000

    beginning tests

--- EPICTaxi/src/test/Testing.java
+++ EPICTaxi/src/test/Testing.java
@@ -1,6 +1,8 @@
 
 //import org.junit.Test;
-import main.java.org.example.Main;
+import org.example.LinkedGrid;
+import org.example.Main;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -12,4 +14,17 @@ public class Testing  {
     public void test1() {
         assertEquals((Main.add(1, 2)), 3);
     }
+    // Inserts the vehicle with registration number reg to the map at location loc if it has not been already added to map.
+// It should return false if the vehicle is not registered or is already on map
+   // public abstract boolean testAddToMap(String reg, Location loc);
+
+
+   // @Test
+   // public boolean testAddToMap(String reg, int x , int y){
+
+
+
+ //   }
+
+
 }
